13b7457151bf674bee8ef01ac688d030a8cf90eb
RobsonMT <rbsndev3@gmail.com>
2022-02-16 13:31:19 -0300

    feat: extra repetition structures

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# Código do dev aqui
+def corresponding_parenthesis(text : str) -> str:
+    parenthesis = ['()',')(']
+
+    while any(x in text for x in parenthesis):
+        for corresponding in parenthesis:
+            text = text.replace(corresponding, '')
+
+    return text
+
+def remove_more_than_two_repetitions(text : str) -> str:
+    text_without_repetitions = ""
+
+    for i, letter in enumerate(text):
+        if letter != text[i -1] or letter != text[i-2]:
+            text_without_repetitions += letter
+
+    return text_without_repetitions
